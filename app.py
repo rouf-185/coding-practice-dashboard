5a9d675015c1329b9c1a7bf4556f66fdf4c43055
rouf-185 <abdurroufbd97@gmail.com>
2026-01-25 20:54:14 -0500

    broadcasted

--- app.py
+++ app.py
@@ -714,5 +714,5 @@ def migrate_database():
 if __name__ == '__main__':
     with app.app_context():
         migrate_database()
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
 
